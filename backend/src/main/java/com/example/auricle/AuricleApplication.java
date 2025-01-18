fed42c8449baeae18fc82602981f00d06b0f0006
HenryKim12 <henrykim966@gmail.com>
2025-01-18 12:23:20 -0800

    init backend spring

---
+++ backend/src/main/java/com/example/auricle/AuricleApplication.java
@@ -0,0 +1,13 @@
+package com.example.auricle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuricleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuricleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/auricle/AuricleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auricle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuricleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
