445b339f3fc97ff0f6ddd624f8d5323b5bb6a266
HenryKim12 <henrykim966@gmail.com>
2025-01-18 23:54:58 -0800

    fixed app.py typo

--- backend/app.py
+++ backend/app.py
@@ -16,5 +16,5 @@ app.register_blueprint(auricle_service, url_prefix='/auricle')
 def hello():
     return 'Hello, World!'
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
