434d86a4e1b30a045b2b429d84d5ddf81d114d91
Noel Illing <noel@netnow.io>
2025-01-18 18:02:51 -0800

    format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Feature from './pages/Feature'
 
-
 function App() {
   return (
     <>
       <Router>
-		    <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="feature" element={<Feature />} />
-		    </Routes>
-	    </Router>
+        </Routes>
+      </Router>
     </>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button"
-import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <div>Home
-    <Button>Click me</Button>
-    <Link to="/feature">Go to Feature Page</Link>
-  </div>
+  return (
+    <div>
+      Home
+      <Button>Click me</Button>
+      <Link to="/feature">Go to Feature Page</Link>
+    </div>
+  )
 }
 
 export default Home
